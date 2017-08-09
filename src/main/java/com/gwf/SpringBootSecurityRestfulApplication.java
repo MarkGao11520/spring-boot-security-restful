5e1d37a2a824305ca4f7418bb386291d9f42f7d8
gwf <18722042639@163.com>
2017-08-09 14:19:28 +0800

    初始化导入

---
+++ src/main/java/com/gwf/SpringBootSecurityRestfulApplication.java
@@ -0,0 +1,12 @@
+package com.gwf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityRestfulApplication.class, args);
+	}
+}
